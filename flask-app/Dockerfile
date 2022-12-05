203edba6fa7005d381fd5dfd8322f21165d84274
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2022-12-05 21:55:57 +0000

    Revert "containerising flask app"
    This reverts commit 20b21af1787dbd115bcffe7ca756684eaa94d197.

--- flask-app/Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:latest
-COPY . .
-RUN pip install -r requirements.txt
-EXPOSE 5000
-ENTRYPOINT ["python", "app.py"]
--- flask-app/application/categories.py
+++
@@ -1,3 +0,0 @@
-# Add categories to this list or delete unneeded ones.
-
-categories = ["Action/Adventure", "Biographies/Autobiographies", "Classics", "Comic Book/Graphic Novel", "Detective, Mystery, & True Crime", "Essays", "Fantasy", "Fiction", "History", "Horror", "Poetry", "Science Fiction (Sci-Fi)", "Self-Help", "Short Stories", "Suspense/Thrillers"]
