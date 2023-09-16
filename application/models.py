c2cdf4ec6545783aa1fc2d781f5ce75d4bbbaac1
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2023-09-16 21:14:42 +0100

    first login form for user

--- application/models.py
+++ application/models.py
@@ -1,18 +1,34 @@
 from application import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     author_name = db.Column(db.String(50), nullable=False)
     books = db.relationship("Book", backref="author")
 
+
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     book_title = db.Column(db.String(50), nullable=False)
     available = db.Column(db.Boolean, default=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+    author_id = db.Column(db.Integer, db.ForeignKey("author.id"))
     category_id = db.Column(db.Integer, db.ForeignKey("category.id"))
+
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     cat_name = db.Column(db.String(50))
     books = db.relationship("Book", backref="category")
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
--- application/routes.py
+++ application/routes.py
@@ -1,35 +1,36 @@
 from application import app, db
-from application.forms import AddAuthor, AddBook, UpdateBook, UpdateAuthor
-from application.models import Author, Book, Category
-from flask import request, render_template, redirect, url_for
+from application.forms import AddAuthor, AddBook, UpdateBook, UpdateAuthor, LoginForm
+from application.models import Author, Book, Category, User
+from flask import request, render_template, redirect, url_for, flash
+from flask_login import login_user, login_required, logout_user, current_user
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template("home.html")
 
 
-@app.route('/add-author', methods=['GET', 'POST'])
+@app.route("/add-author", methods=["GET", "POST"])
 def add_author():
     form = AddAuthor()
-    if request.method == 'POST':
+    if request.method == "POST":
         name = form.author_name.data
         # Checks if author already exists in database
         duplicate = Author.query.filter_by(author_name=name).first()
         # If they don't, the author is added
         if not duplicate:
-            author = Author(author_name = name)
+            author = Author(author_name=name)
             db.session.add(author)
             db.session.commit()
-            return redirect(url_for('add_book'))
+            return redirect(url_for("add_book"))
         # This part may need to be changed, but the basic functionality works
         else:
             return f"That author already exists in the database."
-    return render_template('add_author.html', form=form)
+    return render_template("add_author.html", form=form)
 
 
-@app.route('/add-book', methods=['GET', 'POST'])
+@app.route("/add-book", methods=["GET", "POST"])
 def add_book():
     form = AddBook()
     authors = Author.query.all()
@@ -38,7 +39,7 @@ def add_book():
         form.author.choices.append((author.id, author.author_name))
     for category in categories:
         form.category.choices.append((category.id, category.cat_name))
-    if request.method == 'POST':
+    if request.method == "POST":
         title = form.book_title.data
         author_id = form.author.data
         category_id = form.category.data
@@ -46,30 +47,30 @@ def add_book():
         duplicate = Book.query.filter_by(author_id=author_id, book_title=title).first()
         # If it doesn't, the book is added
         if not duplicate:
-            book = Book(book_title = title, author_id = author_id, category_id = category_id)
+            book = Book(book_title=title, author_id=author_id, category_id=category_id)
             db.session.add(book)
             db.session.commit()
         # This part may need to be changed, but the basic functionality works
         else:
             return f"That book already exists in the database."
-    return render_template('add_book.html', form=form)
+    return render_template("add_book.html", form=form)
 
 
-@app.route('/view-books', methods=['GET'])
+@app.route("/view-books", methods=["GET"])
 def view_books():
     authors = Author.query.all()
     books = Book.query.all()
-    return render_template('view_books.html', authors=authors, books=books)
+    return render_template("view_books.html", authors=authors, books=books)
 
 
-@app.route('/update-book/<int:bid>', methods=['GET','POST'])
+@app.route("/update-book/<int:bid>", methods=["GET", "POST"])
 def update_book(bid):
     form = UpdateBook()
     book = Book.query.filter_by(id=bid).first()
     categories = Category.query.all()
     for category in categories:
         form.category.choices.append((category.id, category.cat_name))
-    if request.method == 'POST':
+    if request.method == "POST":
         title = form.book_title.data
         category_id = form.category.data
         available = form.available.data
@@ -80,41 +81,41 @@ def update_book(bid):
         book.available = available
         db.session.add(book)
         db.session.commit()
-        return redirect(url_for('view_books'))
-    return render_template('update_book.html', form=form, book=book)
+        return redirect(url_for("view_books"))
+    return render_template("update_book.html", form=form, book=book)
 
 
-@app.route('/delete-book/<int:bid>')
+@app.route("/delete-book/<int:bid>")
 def delete_book(bid):
     book = Book.query.filter_by(id=bid).first()
     db.session.delete(book)
     db.session.commit()
-    return redirect(url_for('view_books'))
+    return redirect(url_for("view_books"))
 
 
-@app.route('/view-authors', methods=['GET'])
+@app.route("/view-authors", methods=["GET"])
 def view_authors():
     authors = Author.query.all()
     authors.sort(key=lambda x: x.author_name)
-    return render_template('view_authors.html', authors=authors)
+    return render_template("view_authors.html", authors=authors)
 
 
-@app.route('/update-author/<int:aid>', methods=['GET','POST'])
+@app.route("/update-author/<int:aid>", methods=["GET", "POST"])
 def update_author(aid):
     form = UpdateAuthor()
     author = Author.query.filter_by(id=aid).first()
-    if request.method == 'POST':
+    if request.method == "POST":
         # If author name is left blank, it is not updated.
         if form.author_name.data:
             name = form.author_name.data
             author.author_name = name
             db.session.add(author)
             db.session.commit()
-        return redirect(url_for('view_authors'))
-    return render_template('update_author.html', form=form, author=author)
+        return redirect(url_for("view_authors"))
+    return render_template("update_author.html", form=form, author=author)
 
 
-@app.route('/delete-author/<int:aid>')
+@app.route("/delete-author/<int:aid>")
 def delete_author(aid):
     author = Author.query.filter_by(id=aid).first()
     books = Book.query.filter_by(author_id=aid).all()
@@ -122,11 +123,27 @@ def delete_author(aid):
     for book in books:
         db.session.delete(book)
     db.session.commit()
-    return redirect(url_for('view_authors'))
+    return redirect(url_for("view_authors"))
 
 
-@app.route('/view-categories', methods=['GET'])
+@app.route("/view-categories", methods=["GET"])
 def view_categories():
     categories = Category.query.all()
     books = Book.query.all()
-    return render_template('view_categories.html', categories=categories, books=books)
+    return render_template("view_categories.html", categories=categories, books=books)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            flash("Logged in successfully!", "success")
+            return redirect(url_for("index"))
+        else:
+            flash("Invalid username or password. Please try again.", "danger")
+    return render_template("login.html", form=form)
