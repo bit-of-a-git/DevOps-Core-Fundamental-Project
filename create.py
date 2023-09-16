7ec8502a429c7bf9cbe2de213e4564a697d05ae9
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2023-09-16 22:19:06 +0100

    Using environment variables to create a user and password, changing the home message depending on whether user is logged in or not.

--- create.py
+++ create.py
@@ -1,6 +1,7 @@
 from application import db, app
 from application.categories import categories
-from application.models import Category
+from application.models import Category, User
+import os
 
 with app.app_context():
     db.drop_all()
@@ -10,6 +11,20 @@ with app.app_context():
     categories.sort()
     # Populates the Category table from the categories file
     for i in categories:
-        newcat = Category(cat_name = i)
+        newcat = Category(cat_name=i)
         db.session.add(newcat)
         db.session.commit()
+
+    username = os.getenv("LIBRARIAN_USERNAME")
+    password = os.getenv("LIBRARIAN_PASSWORD")
+
+    if username and password:
+        user = User(username=username)
+        # Set the password for the user (remember to hash it)
+        user.set_password(password)
+
+    # Add the user to the database
+    db.session.add(user)
+    db.session.commit()
+
+    print("User 'Librarian' created.")
