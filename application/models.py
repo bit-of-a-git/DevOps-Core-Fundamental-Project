2e1076cc3ea1602f68422ef881530f6c03a86109
doc335 <92552641+bit-of-a-git@users.noreply.github.com>
2023-09-16 21:36:02 +0100

    adding get_id and is_active as required by Flask-Login

--- application/models.py
+++ application/models.py
@@ -26,9 +26,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return str(self.id)
